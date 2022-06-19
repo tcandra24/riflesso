f4d5e68e8e915a56ae79d390ac4900b4408c37ca
Tito Candra <tcandra007@gmail.com>
2022-06-19 12:45:15 +0700

    Initial Commit Fresh riflesso

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+import { ref } from "@vue/reactivity";
+
+const isPlaying = ref(false)
+const showResult = ref(false)
+const delay = ref(null)
+const score = ref(null)
+
+function start() {
+  delay.value = 2000 + Math.random() * 5000
+  isPlaying.value = true
+  showResult.value = false
+}
+
+function endGame(value) {
+  score.value = value
+  isPlaying.value = false
+  showResult.value = true
+}
+ 
+</script>
+
+<template>
+  <h1>
+    Ninja Reaction Timer
+  </h1>
+  <button @click="start()" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <!-- <p v-if="showResult">Reaction time: {{ score }} ms </p> -->
+  <Results v-if="showResult" :score="score" />
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, ref } from "@vue/runtime-core"
+
+const props = defineProps(['score'])
+const rank = ref('');
+
+onMounted(() => {
+  if (props.score < 205) {
+    rank.value = 'Ninja Fingers'
+  } else if (props.score < 400) {
+    rank.value = 'Rapid Reflexes'
+  } else {
+    rank.value = 'Snail pace....'
+  }
+})
+</script>
+
+<template>
+  <p>Reaction time - {{ props.score }} ms</p>
+  <p>{{ rank }}</p>
+</template>
